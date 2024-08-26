690981a2774a9d4ae4bfed1c673c18a59eb71ef5
Keshav  Shukla <86261066+Keshavopcoder@users.noreply.github.com>
2024-08-26 14:56:35 +0530

    Assignment1(c).java

---
+++ Q.3.java
@@ -0,0 +1,11 @@
+public class CheckOddEvenNumber {
+    public static void main(String[] args) {
+        int num = 7; // predefined number
+        
+        if (num % 2 == 0) {
+            System.out.println("Number is even.");
+        } else {
+            System.out.println("Number is odd.");
+        }
+    }
+}
