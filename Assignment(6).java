84fc619377264837992a4e6253eab904f732975b
Keshav  Shukla <86261066+Keshavopcoder@users.noreply.github.com>
2024-08-26 14:59:41 +0530

    Create Assignment(6).java

---
+++ Assignment(6).java
@@ -0,0 +1,9 @@
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        int length = 5; // predefined length
+        int width = 3; // predefined width
+        
+        int area = length * width;
+        System.out.println("Area of the rectangle: " + area);
+    }
+}
