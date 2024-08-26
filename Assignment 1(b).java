5c3438e50abff2562d35d6209918a5d85f00c72c
Keshav  Shukla <86261066+Keshavopcoder@users.noreply.github.com>
2024-08-26 14:55:35 +0530

    Create Assignment 1(b).java

---
+++ Assignment 1(b).java
@@ -0,0 +1,11 @@
+public class CheckNegativeNumber {
+    public static void main(String[] args) {
+        int num = -5; // predefined number
+        
+        if (num < 0) {
+            System.out.println("Number is negative.");
+        } else {
+            System.out.println("Number is not negative.");
+        }
+    }
+}
