e829969ccdb76cb8e2208d491d8374716266a832
Keshav  Shukla <86261066+Keshavopcoder@users.noreply.github.com>
2024-08-26 14:57:52 +0530

    Create Assignment(e).java

---
+++ Assignment(e).java
@@ -0,0 +1,8 @@
+public class AreaOfSquare {
+    public static void main(String[] args) {
+        int side = 4; // predefined side length
+        
+        int area = side * side;
+        System.out.println("Area of the square: " + area);
+    }
+}
